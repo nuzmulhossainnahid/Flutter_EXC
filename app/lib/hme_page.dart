3daf4c8f2b7dfb464a8e69d7bc3ac9894ca9185e
nuzmulhossainnahid <hasannahidnazmul@gmail.com>
2021-09-16 12:44:43 +0600

    ROW,COLUM

--- app/lib/hme_page.dart
+++ app/lib/hme_page.dart
@@ -7,21 +7,59 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.red,
-        title: Text(
-          "Hellow",
-          style:
-              TextStyle(color: Colors.black, fontSize: 28, letterSpacing: 5.0),
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: Text(
+            "Hellow",
+            style: TextStyle(
+                color: Colors.black, fontSize: 28, letterSpacing: 5.0),
+          ),
         ),
-      ),
-      body: Center(
-        child: Icon(
-          Icons.camera_alt,
-          size: 200,
-          color: Colors.red,
-        ),
-      ),
-    );
+        body:
+            // Row(
+            //   children: [
+            //     Container(
+            //       color: Colors.orange,
+            //       child: FlutterLogo(
+            //         size: 60.0,
+            //       ),
+            //     ),
+            //     Container(
+            //       color: Colors.blue,
+            //       child: FlutterLogo(
+            //         size: 60.0,
+            //       ),
+            //     ),
+            //     Container(
+            //       color: Colors.purple,
+            //       child: FlutterLogo(
+            //         size: 60.0,
+            //       ),
+            //     ),
+            //   ],
+            // ),
+
+            Column(
+          children: [
+            Container(
+              color: Colors.orange,
+              child: FlutterLogo(
+                size: 60.0,
+              ),
+            ),
+            Container(
+              color: Colors.blue,
+              child: FlutterLogo(
+                size: 60.0,
+              ),
+            ),
+            Container(
+              color: Colors.purple,
+              child: FlutterLogo(
+                size: 60.0,
+              ),
+            ),
+          ],
+        ));
   }
 }
