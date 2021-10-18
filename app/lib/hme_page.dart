8356b1b5367156ffcaf61531e4da1634cd323e23
nuzmulhossainnahid <hasannahidnazmul@gmail.com>
2021-10-18 23:15:13 +0600

    Update hme_page.dart

--- app/lib/hme_page.dart
+++ app/lib/hme_page.dart
@@ -47,12 +47,12 @@ class HomePage extends StatelessWidget {
                 ),
               ),
             ]),
-            // Container(
-            //   color: Colors.blue,
-            //   child: FlutterLogo(
-            //     size: 100,
-            //   ),
-            // ),
+            Container(
+              color: Colors.blue,
+              child: FlutterLogo(
+                size: 100,
+              ),
+            ),
           ],
         ),
       ),
